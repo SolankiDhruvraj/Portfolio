5ce697a9c2d817e4fef7460978e02cf51692950e
anonymousa320 <dhruvrajsolanki737@gmail.com>
2023-11-10 09:45:13 +0530

    Changes in Contact and Project

--- src/components/Workcard.js
+++ src/components/Workcard.js
@@ -17,9 +17,37 @@ const Workcard = () => {
             </div>
             <div className="card">
                 <div className="projectCard">
-                    <img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
-                    <h2 className='projectTitle'>Automated System for College Prediction</h2>
-                    <p className='projectDetails'>The user gets the list of Top 10 colleges that he/she can get based on his/her rank.</p>
+                    <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
+                    <h2 className='projectTitle'>Proficient Exam Monitoring System Using Deep Learning Techniques</h2>
+                    <p className='projectDetails'>Proctox is a comprehensive online exam proctoring app that uses real-time video monitoring and AI-based behavior analysis to ensure the integrity of remote exams.
+</p>
+                    <div className="projectSourcebtn">
+                        <div className="viewBtn">
+                            <a href="https://github.com/SolankiDhruvraj/Proctox" target='blank'>View Project</a>
+                        </div>
+                        <div className='sourceBtn'>
+                            <a href="https://github.com/SolankiDhruvraj/Proctox" target='blank'>View Source Code</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="projectCard">
+                    <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
+                    <h2 className='projectTitle'>Deepfake Detection</h2>
+                    <p className='projectDetails'>The project's goal is to detect the presence of such deceptive content to help combat misinformation, fraudulent activities, and protect against privacy breaches.</p>
+                    <div className="projectSourcebtn">
+                        <div className="viewBtn">
+                            <a href="https://github.com/SolankiDhruvraj/Deepfake_Detection" target='blank'>View Project</a>
+                        </div>
+                        <div className='sourceBtn'>
+                            <a href="https://github.com/SolankiDhruvraj/Deepfake_Detection" target='blank'>View Source Code</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="projectCard">
+                    <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
+                    <h2 className='projectTitle'>Machine Learning based Prediction Model for College Admission</h2>
+                    <p className='projectDetails'>A Machine-Learning-based Prediction Model for College Admission is an algorithmic model that utilizes machine learning techniques to predict the probability of admission of a college student to a particular college or university.
+</p>
                     <div className="projectSourcebtn">
                         <div className="viewBtn">
                             <a href="https://github.com/SolankiDhruvraj/Machine-Learning-based-Prediction-Model-for-College-Admission" target='blank'>View Project</a>
@@ -30,12 +58,25 @@ const Workcard = () => {
                     </div>
                 </div>
                 <div className="projectCard">
-                    <img src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
-                    <h2 className='projectTitle'>Codecraft</h2>
-                    <p className='projectDetails'>Codecraft is a social development environment that allows developers to showcase their web development projects, create and share code snippets, and collaborate with others in the community.</p>
+                    <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
+                    <h2 className='projectTitle'>Lawn Tennis Data Analysis</h2>
+                    <p className='projectDetails'>Lawn tennis data analysis using Python involves processing and analyzing player statistics, match outcomes, and game dynamics. It leverages Python's data manipulation and visualization libraries to gain insights into player performance, game trends, and strategy, assisting in performance improvement and strategic decision-making in the sport.</p>
                     <div className="projectSourcebtn">
                         <div className="viewBtn">
-                            <a href="https://codecraftpro.netlify.app" target='blank'>View Project</a>
+                            <a href="https://github.com/SolankiDhruvraj/Lawn-Tennis-Data-Analysis" target='blank'>View Project</a>
+                        </div>
+                        <div className='sourceBtn'>
+                            <a href="https://github.com/SolankiDhruvraj/Lawn-Tennis-Data-Analysis" target='blank'>View Source Code</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="projectCard">
+                    <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
+                    <h2 className='projectTitle'>CodeCraft: The Ultimate HTML, CSS, and JavaScript Code Editor</h2>
+                    <p className='projectDetails'>Create and customize web projects with ease using our HTML, CSS, and JavaScript based code editor. Our user-friendly interface provides a seamless coding experience with features such as real-time previews, syntax highlighting and error highlighting.</p>
+                    <div className="projectSourcebtn">
+                        <div className="viewBtn">
+                            <a href="https://github.com/SolankiDhruvraj/CodeCraft" target='blank'>View Project</a>
                         </div>
                         <div className='sourceBtn'>
                             <a href="https://github.com/SolankiDhruvraj/CodeCraft" target='blank'>View Source Code</a>
@@ -45,7 +86,7 @@ const Workcard = () => {
                 <div className="projectCard">
                     <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
                     <h2 className='projectTitle'>Weather API App</h2>
-                    <p className='projectDetails'>This Projects shows the current weather of that particular city that the user wish to see</p>
+                    <p className='projectDetails'>A weather app project is a software application that displays current and future weather information for a specified location. The app usually retrieves weather data ### from a third-party API and provides information such as temperature. The app is made with ReactJs. The aim of the project is to provide users with accurate and up-to-date weather information in a user-friendly and visually appealing manner.</p>
                     <div className="projectSourcebtn">
                         <div className="viewBtn">
                             <a href="https://github.com/SolankiDhruvraj/Weather-App" target='blank'>View Project</a>
@@ -57,40 +98,29 @@ const Workcard = () => {
                 </div>
                 <div className="projectCard">
                     <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
-                    <h2 className='projectTitle'>Weather API App</h2>
-                    <p className='projectDetails'>This Projects shows the current weather of that particular city that the user wish to see</p>
+                    <h2 className='projectTitle'>Amazon Clone</h2>
+                    <p className='projectDetails'>I developed an Amazon clone with the intention of gaining a deeper understanding of React.js and its capabilities. This project allowed me to explore various aspects of React, such as its component-based architecture, state management, and user interface development, while replicating the core features and functionality of the Amazon website. Through this hands-on experience, I was able to enhance my skills and knowledge in web development using React.js, a popular and powerful JavaScript library for building user interfaces.
+
+</p>
                     <div className="projectSourcebtn">
                         <div className="viewBtn">
-                            <a href="https://github.com/SolankiDhruvraj/Weather-App" target='blank'>View Project</a>
+                            <a href="https://github.com/SolankiDhruvraj/amazon-clone" target='blank'>View Project</a>
                         </div>
                         <div className='sourceBtn'>
-                            <a href="https://github.com/SolankiDhruvraj/Weather-App" target='blank'>View Source Code</a>
+                            <a href="https://github.com/SolankiDhruvraj/amazon-clone" target='blank'>View Source Code</a>
                         </div>
                     </div>
                 </div>
                 <div className="projectCard">
                     <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
-                    <h2 className='projectTitle'>Weather API App</h2>
-                    <p className='projectDetails'>This Projects shows the current weather of that particular city that the user wish to see</p>
+                    <h2 className='projectTitle'>Portfolio Website</h2>
+                    <p className='projectDetails'>My website is designed as a showcase for my portfolio, where I exhibit my collection of creative projects and work.</p>
                     <div className="projectSourcebtn">
                         <div className="viewBtn">
-                            <a href="https://github.com/SolankiDhruvraj/Weather-App" target='blank'>View Project</a>
+                            <a href="https://github.com/SolankiDhruvraj/Portfolio" target='blank'>View Project</a>
                         </div>
                         <div className='sourceBtn'>
-                            <a href="https://github.com/SolankiDhruvraj/Weather-App" target='blank'>View Source Code</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="projectCard">
-                    <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" className='computerImg' />
-                    <h2 className='projectTitle'>Weather API App</h2>
-                    <p className='projectDetails'>This Projects shows the current weather of that particular city that the user wish to see</p>
-                    <div className="projectSourcebtn">
-                        <div className="viewBtn">
-                            <a href="https://github.com/SolankiDhruvraj/Weather-App" target='blank'>View Project</a>
-                        </div>
-                        <div className='sourceBtn'>
-                            <a href="https://github.com/SolankiDhruvraj/Weather-App" target='blank'>View Source Code</a>
+                            <a href="https://github.com/SolankiDhruvraj/Portfolio" target='blank'>View Source Code</a>
                         </div>
                     </div>
                 </div>
